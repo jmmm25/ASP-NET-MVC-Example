d7252e5b2493e5d0fcd1af48ac0df5134f3b3f00
김정민 <jmkim9302@gmail.com>
2022-05-14 00:48:55 +0900

    Add test BoardController

--- Application/App_Start/RouteConfig.cs
+++ Application/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Application
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Alpha", action = "Beta", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Application/Controllers/BoardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public string List(int? Id)
+        {
+            if (Id == null)
+                return "Error Message #1";
+
+            return "Board Id : " + Id.Value;
+        }
+    }
+}
