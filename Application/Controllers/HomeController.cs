86c80ad6596a3a54ac028c3b3edd03940bddb608
김정민 <jmkim9302@gmail.com>
2022-05-16 21:05:25 +0900

    Add Razor example with MVC

--- Application/Controllers/HomeController.cs
+++ Application/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,22 @@ namespace Application.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "My First Application!";
+            //ViewBag.Arr = new string[]
+            //{
+            //    "alpha",
+            //    "beta",
+            //    "gamma"
+            //};
+
+            ViewBag.Book = new Book
+            {
+                Title = "칼의 노래",
+                Writer = "김훈"
+            };
+
+            return View();
         }
     }
 }
